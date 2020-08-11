b22daf38d756c3a5b30f440d9466f99faa52d3ac
Jose Pedro <pedroosd28@gmail.com>
2020-08-10 22:45:35 -0300

    Making Deploy

--- app/config/db.js
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV == "production"){
-  module.exports = {mongoURI: 'mongodb+srv://economize:economize@cluster0.mazu5.mongodb.net/<dbname>?retryWrites=true&w=majority'};
-}else{
-  module.exports = {mongoURI: 'mongodb://localhost/economize'}
-}
